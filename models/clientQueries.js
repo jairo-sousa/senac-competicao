954a74b6319a72dabb49475d0ffc50a57d55f0f0
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-05-28 13:55:31 -0300

    feat: set cliente queries

--- models/clientQueries.js
+++ models/clientQueries.js
@@ -2,4 +2,21 @@ const getAll = `
     SELECT * FROM cliente;
 `
 
-module.exports = { getAll }
+const post = `
+    INSERT INTO cliente SET ?;
+`
+
+const update = `
+    UPDATE cliente SET ? WHERE id = ?;
+`
+
+const remove = `
+    DELETE FROM cliente WHERE id = ?
+`
+
+module.exports = { 
+    getAll,
+    post,
+    update,
+    remove
+ }
